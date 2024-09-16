8bc2c7b1af08047c40cc1ee9659e5c887608ff0e
dityaww <111202012838@mhs.dinus.ac.id>
2024-09-16 15:16:32 +0700

    main pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
+      <h1>MAIN PAGES</h1>
       <Button>Click Me</Button>
     </div>
   );
