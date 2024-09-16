816473c31c577f3d6f993d25fa1c33a689c84180
dityaww <111202012838@mhs.dinus.ac.id>
2024-09-16 15:17:28 +0700

    add layout signin

---
+++ src/app/dashboard/(auth)/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import '../../globals.css'
+
+import { Inter } from 'next/font/google'
+ 
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const metadata: Metadata = {
+  title: "Dashboard | Sign In",
+};
+
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
