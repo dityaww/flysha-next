52edafe1d82af779696ece9cf76740fea3ad9fb2
dityaww <111202012838@mhs.dinus.ac.id>
2024-12-03 16:50:19 +0700

    add meta data ft airplane

--- src/app/dashboard/(home)/airplanes/page.tsx
+++ src/app/dashboard/(home)/airplanes/page.tsx
@@ -5,27 +5,32 @@ import Link from 'next/link'
 import React from 'react'
 import { columns } from './components/columns-table'
 import { getAirplanes } from './lib/data'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: "Dashboard | Airplanes"
+}
 
 async function AirplanePage() {
     const planes = await getAirplanes()
-
-  return (
-    <>
-        <div className="flex flex-row items-center justify-between">
-            <div className="my-5 text-2xl font-bold">
-                Airplanes
+    
+    return (
+        <>
+            <div className="flex flex-row items-center justify-between">
+                <div className="my-5 text-2xl font-bold">
+                    Airplanes
+                </div>
+                <Button asChild>
+                    <Link href={'/dashboard/airplanes/create'}>
+                        <Plus className='mr-2 h-4 w-4' />
+                        Tambah Data
+                    </Link>
+                </Button>
             </div>
-            <Button asChild>
-                <Link href={'/dashboard/airplanes/create'}>
-                    <Plus className='mr-2 h-4 w-4' />
-                    Tambah Data
-                </Link>
-            </Button>
-        </div>
 
-        <DataTable columns={columns} data={planes} />
-    </>
-  )
+            <DataTable columns={columns} data={planes} />
+        </>
+    )
 }
 
 export default AirplanePage
