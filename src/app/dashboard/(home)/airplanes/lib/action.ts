a6b02e523d3592f998eb594e80de0dd53576a8a9
dityaww <111202012838@mhs.dinus.ac.id>
2024-12-03 16:53:21 +0700

    set action for delete, update, and get byId

--- src/app/dashboard/(home)/airplanes/lib/action.ts
+++ src/app/dashboard/(home)/airplanes/lib/action.ts
@@ -3,9 +3,24 @@
 import { ActionResult } from "@/app/dashboard/(auth)/login/form/action";
 import { airplaneFormSchema } from "./validation";
 import { redirect } from "next/navigation";
-import { uploadFile } from "@/lib/supabase";
+import { deleteFile, uploadFile } from "@/lib/supabase";
 import prisma from "../../../../../../lib/prisma";
 import { revalidatePath } from "next/cache";
+
+export async function getAirplaneById(id: string) {
+    try {
+        const data = await prisma.airplane.findFirst({
+            where: {
+                id: id
+            }
+        })
+
+        return data
+    } catch (error) {
+        console.log(error)
+        return null
+    }
+}
 
 export async function saveAirplane(prevState: any, formData: FormData): Promise<ActionResult> {
     const values = airplaneFormSchema.safeParse({
@@ -49,4 +64,121 @@ export async function saveAirplane(prevState: any, formData: FormData): Promise<
 
     revalidatePath('/dashboard/airplanes')
     redirect('/dashboard/airplanes')
+}
+
+export async function updateAirplane(
+    prevState: unknown,
+    id: string,
+    formData: FormData
+): Promise<ActionResult> {
+    const image = formData.get('image') as File
+
+    let airplaneFormSchemaUpdate;
+
+    if (image.size === 0) {
+        airplaneFormSchemaUpdate = airplaneFormSchema.omit({image: true})
+    } else {
+        airplaneFormSchemaUpdate = airplaneFormSchema
+    }
+
+    const values = airplaneFormSchemaUpdate.safeParse({
+        name: formData.get('name'),
+        image: formData.get('image'),
+        code: formData.get('code'),
+    })
+
+    if (!values.success) {
+        const errorDesc = values.error.issues.map(issue => issue.message)
+
+        return {
+            errorTitle: 'Error Validation',
+            errorDesc
+        }
+    }
+
+    let filename: unknown
+
+    if (image.size > 0) {
+        const uploadedFile = await uploadFile(image)
+
+        if (uploadedFile instanceof Error) {
+            return {
+                errorTitle: 'Failed to upload file',
+                errorDesc: ['Terjadi kesalahan pada koneksi, silahkan coba lagi']
+            }
+        }
+
+        filename = uploadedFile as string
+    } else {
+        const airplane = await prisma.airplane.findFirst({
+            where: {
+                id: id
+            },
+            select: {
+                image: true
+            }
+        })
+
+        filename = airplane?.image
+    }
+
+    try {
+        await prisma.airplane.update({
+            where: {
+                id: id
+            },
+            data: {
+                name: values.data.name,
+                code: values.data.code,
+                image: filename as string
+            }
+        })
+    } catch (error) {
+        return {
+            errorTitle: 'Failed to update data',
+            errorDesc: ['Terjadi kesalahan pada koneksi, silahkan coba lagi']
+        }
+    }
+
+    revalidatePath('/dashboard/airplanes')
+    redirect('/dashboard/airplanes')
+}
+
+export async function deleteAirplane(id: string): Promise<ActionResult | undefined> {
+    const data = await prisma.airplane.findFirst({
+        where: {
+            id: id
+        }
+    })
+
+    if (!data) {
+        return {
+            errorTitle: "Data not found",
+            errorDesc: []
+        }
+    }
+
+    const deletedFile = await deleteFile(data?.image)
+
+    if (deletedFile instanceof Error) {
+        return {
+            errorTitle: "Failed to delete file",
+            errorDesc: ['Terjadi kesalahan pada koneksi, silahkan coba lagi']
+        }
+    }
+
+    try {
+        await prisma.airplane.delete({
+            where: { id: id }
+        })
+    } catch (error) {
+        console.log(error)
+
+        return {
+            errorTitle: "Failed to delete data",
+            errorDesc: ['Terjadi kesalahan pada koneksi, silahkan coba lagi']
+        }
+    }
+
+    revalidatePath('/dashboard/airplanes')
 }
